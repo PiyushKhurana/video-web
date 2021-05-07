e09f130ba122be742497b540b37fa338fc6dad04
PiyushKhurana <khuranapiyush27@gmail.com>
2021-05-07 19:20:14 +0530

    config to use env var for dbname

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
+
 require __DIR__ . '/vendor/autoload.php';
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
-$host     =  $_ENV['HOST'];  /* Host name */
-$user     =  $_ENV['DBUSER'];      /* User */
-$password =  $_ENV['PASSWORD'];        /* Password */
-$dbname   =  'crondbv1';  /* Database name */
+$host     =  $_ENV['HOST'];        /* Host name     */
+$user     =  $_ENV['DBUSER'];      /* User          */
+$password =  $_ENV['PASSWORD'];    /* Password      */
+$dbname   =  $_ENV['DBNAME'];      /* Database name */
 
 // Create connection
 $con = mysqli_connect($host, $user, $password,$dbname);
